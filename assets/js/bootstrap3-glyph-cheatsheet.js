286d9b904d45a0b010dccb3817b7786cca04dffd
jamescroft <james.croft@gmail.com>
2014-01-02 13:08:22 +1000

    Decoupled CSS and JS from main page

---
+++ assets/js/bootstrap3-glyph-cheatsheet.js
@@ -0,0 +1,47 @@
+/* Bootstrap Cheat Sheets
+   
+   Bootstrap 3 Glyphs
+   
+   By: James Croft (@jamescroft on Twitter)
+   
+   Contribute or fork this project.  
+ */
+
+// Animated scroll to page anchors via nav
+
+$(document).ready(function (){
+	$("#scroll-to-about").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#about").offset().top
+		}, 500);
+	});
+	$("#scroll-to-home").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#home").offset().top
+		}, 500);
+	});
+});
+
+// Using ZeroClipboard to copy glyph codes to clipboard.
+
+var clip = new ZeroClipboard( $('.copy-button'), {
+  moviePath: "zeroclipboard/ZeroClipboard.swf"
+} );
+
+clip.on( "load", function(client) {
+  // alert( "movie is loaded" );
+
+  client.on( "complete", function(client, args) {
+	  
+    // `this` is the element that was clicked
+    that = this;
+	$(that).text("Copied!");
+	$(that).removeClass("zeroclipboard-is-hover", 0, "linear");
+	$(that).addClass("btn-success", 100, "linear");	
+    setTimeout(function() {
+        $(that).removeClass("btn-success", 100, "linear"), 
+		$(that).text("Copy");
+    }, 600)	
+  });
+});
+
