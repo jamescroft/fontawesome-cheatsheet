c5e2f56bbfcc052bb38d04d8ef560dcc1d0768d5
jamescroft <james.croft@gmail.com>
2014-02-04 18:05:32 +1000

    change asset links, favicon link

--- assets/js/fontawesome-cheatsheet.js
+++ assets/js/fontawesome-cheatsheet.js
@@ -1,11 +1,13 @@
 /* Bootstrap Cheat Sheets
    
-   Glyphicon Cheat Sheet
+   Font Awesome Cheat Sheets
    
    By: James Croft (@jamescroft on Twitter)
    
    Contribute or fork this project.  
  */
+
+
 
 $(window).load(function() {
   // When the page has loaded, reveal
@@ -13,9 +15,10 @@ $(window).load(function() {
   $("#about").css("opacity", 1 );
 });
 
+
 $(document).ready(function (){
 	
-// Animated scroll to page anchors via nav
+	// Animated scroll to page anchors via nav
 
 	var fixedNavHeight = $(".navbar").height();
 
@@ -41,15 +44,14 @@ function scrollToHome() {
     	});
 
 }
+	
+$("#scroll-to-about").click(function() {
+	scrollToAbout();
+});
 
-	
-	$("#scroll-to-about").click(function() {
-		scrollToAbout();
-	});
-	
-	$("#scroll-to-home").click(function() {
-		scrollToHome();
-	});
+$("#scroll-to-home").click(function() {
+	scrollToHome();
+});
 
 //Create rows: 10 icons per row
 
@@ -62,30 +64,28 @@ function createGlyphRows() {
 	for (var c = 10; c<=glyphCount;c+=20) {
 		$("li.grid-icon").slice(c, c+rowSize).wrapAll("<div class='row row-alt'><div class='container'></div></div>");
 	};
-	$("li.grid-icon .btn-container").attr("display","block");
-	$("ul.glyphicons .row:first").attr("id", "first-row");
-	$("ul.glyphicons").css("max-width", "100%");
-	
+	$("ul.font-awesome-icons .row:first").attr("id", "first-row");
+	$("ul.font-awesome-icons").css("max-width", "100%");
 };
 
 createGlyphRows();
 	
-/* Search: https://github.com/DeuxHuitHuit/quicksearch */
-// The search function needs to be timedOut so the classnames can be loaded in first
+ //Search: https://github.com/DeuxHuitHuit/quicksearch 
+ //The search function needs to be timedOut so the classnames can be loaded in first
 
 setTimeout(function() {
 	$('input#glyph-search').quicksearch('li.grid-icon', {
 		 'onBefore': function () {
-        $('ul.glyphicons div.row').hide();
-		$('li.grid-icon').appendTo('#first-row');
+        $('ul.font-awesome-icons div.row').hide();
+		$('li.grid-icon').addClass('temp').appendTo('#first-row');
     },
 		'show': function () {
 			$(this).show();
 			$(this).parent('div.row').show();
 		},
-		'onAfter': function () {	
+		'onAfter': function () {
 			scrollToHome();
-    	}
+   		}
 		});
 }, 100);
 
@@ -99,11 +99,22 @@ $('#glyph-form').bind("keyup keypress", function(e) {
   }
 });
 
-// End Search
+if($('#glyph-search').length) {
+	var $clear = $('#filter-clear');
+	console.log($clear);
+	$clear.click(function(e) {
+		e.preventDefault();
+		$('#glyph-search').val('').trigger('keyup').focus();
+	});
+};
+		
+		
+ //End Search
+
 	
 	//For each of the icons in the grid 
  
-	$(".grid-icon span.glyphicon").each(function(index){		
+	$(".grid-icon i.fa").each(function(index){		
 	//Find the unicode reference in the data-unicode attribute and render it as HTML inside the <i> tag		
 		glyphStore = $(this);
 		if ($(this).attr("data-unicode")) {
@@ -111,32 +122,31 @@ $('#glyph-form').bind("keyup keypress", function(e) {
 		};		
 	//Finds the class of the font awesome icon in the grid and display it underneath		
 		var classCode = glyphStore.attr("class").split(" ");
-		 $(glyphStore).next(".glyphicon-class").text("." + classCode[1]);		 
+		 $(glyphStore).next(".fa-class").text("." + classCode[1]);	
 	});
 	
-	//Adds a break to the glyphicon class prefix, helps with display of class names in grid
-	 $(".glyphicon-class").each(function() {
-		var html = $(this).html().split("-");
-		html = html[0] + "-" + "<br>" + html.slice(1).join("-");
-		$(this).html(html);
-	 });
+	//Adds a break to the font awesome class prefix, helps with display of class names in grid
+		 $(".fa-class").each(function() {
+			var html = $(this).html().split("-");
+			html = html[0] + "-" + "<br>" + html.slice(1).join("-");
+			$(this).html(html);
+		 });
 
- 		
+
 //End of element insertions into page, so initialise scrollspy
 
 $('body').scrollspy({ target: '.navbar', offset: (fixedNavHeight - 1)    });
 
 if (Modernizr.touch) {
-	 $("ul.glyphicons li.grid-icon .btn-container").hide();
+	 $("ul.font-awesome-icons li.grid-icon .btn-container").hide();
 };
 
 }); //End document ready
 
-
 // Using ZeroClipboard to copy glyph codes to clipboard.
 
 var client = new ZeroClipboard( $('.copy-button'), {
-  moviePath: "../assets/zeroclipboard-1.2.3/ZeroClipboard.swf"
+  moviePath: "assets/zeroclipboard-1.2.3/ZeroClipboard.swf"
 } );
 
 client.on( "load", function(client, args) {
@@ -144,7 +154,7 @@ client.on( "load", function(client, args) {
 	client.on( 'mouseover', function ( client, args ) {
 	  //Sets the value to be injected into the clipboard as the item on mouseover
 	  	
-	  glyphStore = $(this).parents("li.grid-icon").children("span.glyphicon");
+	  glyphStore = $(this).parents("li.grid-icon").children("i.fa");
 	  if ($(this).hasClass("parent-copy")) {
 		  //Copy the HTML Tag into the clipboard
 		  //Because of the unicode symbols rendered as contents in the <i> tag, a local clone must be taken and emptied to parse the correct clipboard value
